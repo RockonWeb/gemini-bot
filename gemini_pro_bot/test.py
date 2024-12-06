bd925b79e739ca9f7e76a9b86c2a2013ebe955c1
RockonWeb <96973932+RockonWeb@users.noreply.github.com>
2024-12-06 17:34:12 +0800

    Delete gemini_pro_bot/test.py

--- gemini_pro_bot/test.py
+++
@@ -1,8 +0,0 @@
-import httpx
-
-proxy_url = "http://TQDcLFzD:beBUzDkf@172.252.110.215:64676"
-url = "https://api.telegram.org/bot<YOUR_BOT_TOKEN>/getMe"
-
-with httpx.Client(proxies={"http://": proxy_url, "https://": proxy_url}, timeout=30) as client:
-    response = client.get(url)
-    print(response.json())
